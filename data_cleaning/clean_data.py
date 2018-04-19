20ce0cbdcddf831eed90e2ad642156dcf23e622a
Jessa Rili <jessa.rili@nokia.com>
2018-04-19 11:58:36 +0800

    Added implementation for multiple input files. Made output_filename input as REQUIRED. Sample usage: python3 clean_data.py "English-Tagalog Database - Common.tsv" "English-Tagalog Database - School.tsv" "English-Tagalog Database - Secret.tsv" Group9data_all.txt

--- data_cleaning/clean_data.py
+++ data_cleaning/clean_data.py
@@ -12,49 +12,47 @@ import os
 import re
 import sys
 
-def clean_tsv_data(input_filename = "english-tagalogDB_School.tsv", output_filename = None):
-    if output_filename is None:
-        output_filename = os.path.splitext(input_filename)[0] + "_CLEANED.tsv"
+def clean_tsv_data(input_filenames, output_filename):
     eos_regex = "[\t]+"
 
-    print('Processing input TSV: %s...' % input_filename)
-
-    input_file = open(input_filename, "r", encoding="utf8")
     output_file = open(output_filename, "w+", encoding="utf8")
-    input_file_lines = input_file.readlines()
-    for line in input_file_lines:
-        split_line = re.split(eos_regex, line)
-        entry_number = 0
-        for entry in split_line:
-            if '' == entry.rstrip():
-                continue
-            output_file.write(entry.rstrip())
-            print('.', end='')
-            if 0 == entry_number % 2:
-                output_file.write("\t")
-            else:
-                output_file.write("\n")
-            entry_number = entry_number + 1
+    num_lines_in_output_file = 0
 
-    print('\nProcessing complete! See output TSV: %s' % output_filename)
+    for input_filename in input_filenames:
+        print('\n\nProcessing input TSV: %s...' % input_filename)
+        input_file = open(input_filename, "r", encoding="utf8")
+        input_file_lines = input_file.readlines()
+        for line in input_file_lines:
+            split_line = re.split(eos_regex, line)
+            entry_number = 0
+            for entry in split_line:
+                if '' == entry.rstrip():
+                    continue
+                output_file.write(entry.rstrip())
+                print('.', end='')
+                if 0 == entry_number % 2:
+                    output_file.write("\t")
+                else:
+                    output_file.write("\n")
+                    num_lines_in_output_file = num_lines_in_output_file + 1
+                entry_number = entry_number + 1
+
+    print('\n\nProcessing complete! See output TSV: %s (%d translations)' %(output_filename, num_lines_in_output_file))
 
 def print_usage():
-    print('USAGE: python3 clean_data.py <input.tsv> <output.tsv>')
+    print('USAGE: python3 clean_data.py <input1.tsv> <input2.tsv> ... <output.tsv>')
     print('\t<input.tsv> : filename of the input TSV (REQUIRED)')
-    print('\t<output.tsv> : filename of the output (OPTIONAL)')
+    print('\t<output.tsv> : filename of the output (REQUIRED)')
     print('\t\tDefault: <input.tsv>_CLEANED.tsv')
 
 if __name__ == "__main__":
-    if 1 >= len(sys.argv):
+    if 2 >= len(sys.argv):
         print_usage()
         sys.exit(1)
-    input_filename = sys.argv[1]
-    if 2 < len(sys.argv):
-        output_filename = sys.argv[2]
-    else:
-        output_filename = os.path.splitext(input_filename)[0] + "_CLEANED.tsv"
+    output_filename = sys.argv[len(sys.argv) - 1]
+    input_filenames = sys.argv[1:len(sys.argv) - 1]
 
-    clean_tsv_data(input_filename, output_filename)
+    clean_tsv_data(input_filenames, output_filename)
     sys.exit(0)
 
 
